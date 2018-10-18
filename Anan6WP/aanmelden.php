c414abc37e5f28a72721fa80a81f9e84ef64b96b
liammeijles <36471660+liammeijles@users.noreply.github.com>
2018-10-18 13:53:07 +0200

    form improvements

--- Anan6WP/aanmelden.php
+++ Anan6WP/aanmelden.php
@@ -14,9 +14,9 @@ Text Domain:  wporg
 add_shortcode('dnAanmelden','fDNAanmelden');
 //perform the shortcode output
 function fDNAanmelden($atts, $content = '', $tag){
-     $html  = '<link href="/wp-content/plugins/aanmelden/public/CSS/style.css" rel="stylesheet" type="text/css" />';
+    $html  = '<link href="/wp-content/plugins/aanmelden/public/CSS/style.css" rel="stylesheet" type="text/css" />';
     $html .= '<script src="/wp-content/plugins/aanmelden/public/js/homeAanmelden.js" type="text/javascript"></script>';
-     $html .= '<form action="#">';
+    $html .= '<form action="#">';
     $html .= '<legend>Aanmeldingsformulier deelnemer</legend>';
     $html .= '<div class="entry">';
     $html .= '<label for="dnAkkoord">Voordat je de aanmelding start, is het van belang dat je kennis neemt van en akkoord gaat met onze privacyverklaring. Deze kun je <a href="#">hier</a> lezen en hier accorderen<input type="checkbox" id="dnAkkoord" name="dnAkkoord" value="OK"></label></div>';
@@ -26,7 +26,7 @@ function fDNAanmelden($atts, $content = '', $tag){
     $html .= '<label><input type="radio" name="wie" value="M">Mijzelf</label>';
     $html .= '<label><input type="radio" name="wie" value="A">Iemand die ik begeleid</label></div></div>';
     $html .= '<div class="entry">';
-    $html .= '<label class="red" for="aanEmail">E-mailadres aanmelder</label><input type="text" name="email" id="dnEmail" placeholder="Vergeet niet onze privacyverklaring te accoderen." class="required formatEmail"><button id="dnSubmit" type="submit" disabled="disabled">Aanmelden</button></div>';
+    $html .= '<label class="red" for="aanEmail">E-mailadres aanmelder</label><input type="text" name="email" id="dnEmail" placeholder="Vergeet niet onze privacyverklaring te accorderen." class="required formatEmail"><button id="dnSubmit" type="submit" disabled="disabled">Aanmelden</button></div>';
     $html .= '<p id="dnMessage"></p></form>';
      return $html;
 }
@@ -35,14 +35,15 @@ add_shortcode('vwAanmelden', 'fVWAanmelden');
  function fVWAanmelden() {
     $html  = '<link href="/wp-content/plugins/aanmelden/public/CSS/style.css" rel="stylesheet" type="text/css" />';
     $html .= '<script src="/wp-content/plugins/aanmelden/public/js/homeAanmelden.js" type="text/javascript"></script>';
-     $html .= '<form action="#">';
+    $html .= '<form action="#">';
     $html .= '<legend>Aanmeldingsformulier vrijwilliger</legend>';
     $html .= '<div class="entry">';
-    $html .= '<label for="dnAkkoord">Voordat je de aanmelding start, is het van belang dat je kennis neemt van en akkoord gaat met onze privacyverklaring. Die kun je <a href="#">hier</a> lezen en hier accorderen<input type="checkbox" id="dnAkkoord" name="dnAkkoord" value="OK"></label></div>';
+    $html .= '<label for="dnAkkoord">Op deze pagina kunnen vrijwilligers voor onze instantie zich aanmelden. Je hoeft hier alleen je email in te vullen en akkoord te gaan met onze privacyvoorwaarden, en het aanmeldformulier wordt naar u toegemaild.</label></div>';
     $html .= '<div class="entry">';
     $html .= '<div>';
     $html .= '<div class="entry">';
-    $html .= '<label class="red" for="aanEmail">E-mailadres aanmelder</label><input type="text" name="email" id="dnEmail" placeholder="Vergeet niet onze privacyverklaring te accoderen." class="required formatEmail"><button id="dnSubmit" type="submit" disabled="disabled">Aanmelden</button></div>';
+    $html .= '<input type="text" name="email" id="dnEmail" placeholder="Voer hier uw email adres in" class="required formatEmail"></div>';
+    $html .= '<label for="dnAkkoord"><input type="checkbox" id="dnAkkoord" name="dnAkkoord" value="OK">Ik heb de <a href="#">Privacy voorwaarden</a> gelezen en ga hiermee akkoord.</label><button id="dnSubmit" type="submit" disabled="disabled">Aanmelden</button></div>';
     $html .= '<p id="dnMessage"></p></form>';
      return $html;
 }
